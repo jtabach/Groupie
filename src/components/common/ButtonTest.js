d028e70491613be698d8e2cf1605a9f6ed011a5f
jtabach <jeff.n.tabachnick@gmail.com>
2018-12-07 00:18:13 +0900

    re-add rendered button and styles

--- src/components/common/ButtonTest.js
+++ src/components/common/ButtonTest.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Button.module.scss';
+import _ from 'lodash';
 
-class TestButton extends Component {
+class ButtonTest extends Component {
+  static propTypes = {
+    type: PropTypes.oneOf(['button', 'submit']),
+    variant: PropTypes.oneOf([
+      'primary',
+      'secondary',
+      'secondary-inverse',
+      'outline',
+      'primary-inverse',
+      'green',
+      'green-inverse',
+      'red',
+      'red-inverse'
+    ]),
+    size: PropTypes.oneOf(['large', 'small']),
+    onClick: PropTypes.func,
+    label: PropTypes.string,
+    children: PropTypes.node
+  };
+
+  static defaultProps: {
+    type: 'button',
+    variant: 'primary',
+    size: 'large',
+    label: '',
+    children: null
+  };
 
   render() {
-    return <div>test</div>
+    const { type, variant, size, onClick, label, children } = this.props;
+
+    return (
+      <button
+        type={type}
+        className={`${styles[variant]} ${styles[size]}`}
+        onClick={type === 'submit' ? () => _.noop() : onClick}
+      >
+        {label || children}
+      </button>
+    );
   }
 }
-export default TestButton;
+
+export default ButtonTest;
