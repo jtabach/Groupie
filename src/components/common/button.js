9742c071e783178630266a5646365cfcf9282977
jtabach <jeff.n.tabachnick@gmail.com>
2018-12-06 23:39:12 +0900

    temp test

--- src/components/common/button.js
+++ src/components/common/button.js
@@ -4,34 +4,35 @@ import styles from './Button.module.scss';
 import _ from 'lodash';
 
 class Button extends Component {
-  static propTypes = {
-    type: PropTypes.oneOf(['button', 'submit']),
-    variant: PropTypes.oneOf([
-      'primary',
-      'secondary',
-      'secondary-inverse',
-      'outline',
-      'primary-inverse',
-      'green',
-      'green-inverse',
-      'red',
-      'red-inverse'
-    ]),
-    size: PropTypes.oneOf(['large', 'small']),
-    onClick: PropTypes.func,
-    label: PropTypes.string,
-    children: PropTypes.node
-  };
-
-  static defaultProps: {
-    type: 'button',
-    variant: 'primary',
-    size: 'large',
-    label: '',
-    children: null
-  };
+  // static propTypes = {
+  //   type: PropTypes.oneOf(['button', 'submit']),
+  //   variant: PropTypes.oneOf([
+  //     'primary',
+  //     'secondary',
+  //     'secondary-inverse',
+  //     'outline',
+  //     'primary-inverse',
+  //     'green',
+  //     'green-inverse',
+  //     'red',
+  //     'red-inverse'
+  //   ]),
+  //   size: PropTypes.oneOf(['large', 'small']),
+  //   onClick: PropTypes.func,
+  //   label: PropTypes.string,
+  //   children: PropTypes.node
+  // };
+  //
+  // static defaultProps: {
+  //   type: 'button',
+  //   variant: 'primary',
+  //   size: 'large',
+  //   label: '',
+  //   children: null
+  // };
 
   render() {
+    return null;
     const { type, variant, size, onClick, label, children } = this.props;
 
     return (
