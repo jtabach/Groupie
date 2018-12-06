b5d4def581bbdd8c439410ae4dda485a107540ac
jtabach <jeff.n.tabachnick@gmail.com>
2018-12-06 22:05:40 +0900

    Updates

---
+++ precache-manifest.abcb2bbb4e5b901232beb92cf68b13c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f67e0b3071c9e1803d41",
+    "url": "/groupie/static/js/runtime~main.f67e0b30.js"
+  },
+  {
+    "revision": "a0dd37a7cfde3b7fc7af",
+    "url": "/groupie/static/js/main.a0dd37a7.chunk.js"
+  },
+  {
+    "revision": "ba25a5694677b9df49c5",
+    "url": "/groupie/static/js/1.ba25a569.chunk.js"
+  },
+  {
+    "revision": "a0dd37a7cfde3b7fc7af",
+    "url": "/groupie/static/css/main.1da725d6.chunk.css"
+  },
+  {
+    "revision": "25456e9169d4f11b698979454f796b8f",
+    "url": "/groupie/index.html"
+  }
+];
