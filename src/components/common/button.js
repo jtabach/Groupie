e0f45c754c55152833b5cbc36c5cc49eca916172
jtabach <jeff.n.tabachnick@gmail.com>
2018-12-06 22:47:20 +0900

    include absolute paths from src

--- src/components/common/button.js
+++ src/components/common/button.js
@@ -33,6 +33,7 @@ class Button extends Component {
 
   render() {
     const { type, variant, size, onClick, label, children } = this.props;
+    console.log(`${styles[variant]} ${styles[size]}`);
 
     return (
       <button
