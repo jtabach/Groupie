582f75b5bb0d4052954655d34a3d2f1b006ec19b
jtabach <jeff.n.tabachnick@gmail.com>
2019-02-04 14:32:19 -0500

    register and login - styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,35 +1,42 @@
 import React, { Component } from 'react';
+import styles from './Login.module.scss';
 import { connect } from 'react-redux';
 
 import FormWrapper from './forms/FormWrapper';
 import LoginForm from './forms/LoginForm';
 import Card from './common/Card';
+import NavLayout from './layouts/NavLayout';
 
 import { loginUser } from '../actions/authActions';
 
 class Login extends Component {
-  handleSubmit = values => {
+  handleSubmit = (values) => {
     this.props.loginUser(values);
   };
 
   render() {
     return (
-      <div>
-        <div className="offset-4 col-4">
-          <Card>
-            <FormWrapper
-              formTitle={'Login With Your Email'}
-              form={
-                <LoginForm
-                  onSubmit={this.handleSubmit}
-                  errorMessage={this.props.user.errorMessage}
-                  isLoading={this.props.user.loading}
-                />
-              }
-            />
-          </Card>
+      <NavLayout>
+        <div className={styles['login']}>
+          <div className={styles['login__header']}>
+            <h2>Welcome to Group Fantasy Football League</h2>
+          </div>
+          <div className='offset-4 col-4'>
+            <Card>
+              <FormWrapper
+                formTitle={'Login With Your Email'}
+                form={
+                  <LoginForm
+                    onSubmit={this.handleSubmit}
+                    errorMessage={this.props.user.errorMessage}
+                    isLoading={this.props.user.loading}
+                  />
+                }
+              />
+            </Card>
+          </div>
         </div>
-      </div>
+      </NavLayout>
     );
   }
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import styles from './Register.module.scss';
 import { connect } from 'react-redux';
 
 import FormWrapper from './forms/FormWrapper';
 import RegisterForm from './forms/RegisterForm';
 import Card from './common/Card';
+import NavLayout from './layouts/NavLayout';
 
 import { registerUser } from '../actions/authActions';
 
 class Register extends Component {
-  handleSubmit = values => {
+  handleSubmit = (values) => {
     this.props.registerUser({
       email: values.email,
       password: values.password1,
@@ -19,22 +21,27 @@ class Register extends Component {
 
   render() {
     return (
-      <div>
-        <div className="offset-4 col-4">
-          <Card>
-            <FormWrapper
-              formTitle={'Register With Your Email'}
-              form={
-                <RegisterForm
-                  onSubmit={this.handleSubmit}
-                  errorMessage={this.props.user.errorMessage}
-                  isLoading={this.props.user.loading}
-                />
-              }
-            />
-          </Card>
+      <NavLayout>
+        <div className={styles['register']}>
+          <div className={styles['register__header']}>
+            <h2>Welcome to Group Fantasy Football League</h2>
+          </div>
+          <div className='offset-4 col-4'>
+            <Card>
+              <FormWrapper
+                formTitle={'Register With Your Email'}
+                form={
+                  <RegisterForm
+                    onSubmit={this.handleSubmit}
+                    errorMessage={this.props.user.errorMessage}
+                    isLoading={this.props.user.loading}
+                  />
+                }
+              />
+            </Card>
+          </div>
         </div>
-      </div>
+      </NavLayout>
     );
   }
 }
