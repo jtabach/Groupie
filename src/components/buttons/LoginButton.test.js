9cf1a4aa9685bcb0e1622e9043e60ad431dea75d
jtabach <jeff.n.tabachnick@gmail.com>
2019-03-19 11:54:17 -0500

    test dependencies + snapshot test for LoginButton

---
+++ src/components/buttons/LoginButton.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { StaticRouter } from 'react-router';
+import LoginButton from './LoginButton';
+
+test('LoginButton Link matches snapshot', () => {
+  const component = renderer.create(
+    <StaticRouter location='/' context={{}}>
+      <LoginButton />
+    </StaticRouter>
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
