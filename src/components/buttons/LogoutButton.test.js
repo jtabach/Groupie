bd64ffcb91f81d1f6b6bf449e26da38f50236614
jtabach <jeff.n.tabachnick@gmail.com>
2019-03-19 13:33:28 -0500

    logoutButton - unit and integration test

---
+++ src/components/buttons/LogoutButton.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import { LogoutButton } from './LogoutButton';
+
+describe('LogoutButton', () => {
+  let wrapper;
+  const mockStore = configureStore();
+  const store = mockStore({});
+  const logoutUser = jest.fn();
+
+  beforeEach(() => {
+    wrapper = shallow(<LogoutButton store={store} logoutUser={logoutUser} />);
+  });
+
+  it('renders without crashing', () => {
+    expect(wrapper.length).toEqual(1);
+  });
+
+  it('should execute logout on click', () => {
+    wrapper.find('div').simulate('click');
+    expect(logoutUser).toHaveBeenCalledTimes(1);
+  });
+});
